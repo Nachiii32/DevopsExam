e78dc39db26cae86ce59390f6411d72ba4fdb424
Nachiii32 <hangekarnachiket@gmail.com>
2024-12-29 23:04:56 -0800

    Update SumExample.java

--- SumExample.java
+++ SumExample.java
@@ -1,7 +1,7 @@
 public class SumExample {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 10;
+        int num1 = 6;
+        int num2 = 4;
         int sum = num1 + num2;
         
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
